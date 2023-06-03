73df33244f36bf7d29ce36c3890af117497f9deb
NovaHarry <121847051+NovaHarry@users.noreply.github.com>
2023-06-03 06:22:03 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({
-    message:"Hai"
+    message:"Check Readme file in Git Hub"
   });
 });
 
